fc7c0de58ead2430b1fd97df77d3c9de34cbd0f0
Josiah <legendaryfox@gmail.com>
2012-01-25 09:45:50 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BamftServer::Application.config.secret_token = '9526ec11d1cfe655982fee8ce7cb7f57b030c46747ef379a682457b1507fb8e7315d6c954db07ab9fe0fb532fa2d7bd4bc6330647b94030e93ed36066fb54c4f'
