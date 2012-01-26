2cd52c05491de4897a10ef598e757991ea07f92f
Josiah <legendaryfox@gmail.com>
2012-01-26 00:58:33 -0500

    Heloo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120126052728) do
+ActiveRecord::Schema.define(:version => 20120126055700) do
 
   create_table "datapulls", :force => true do |t|
     t.string   "checksum"
